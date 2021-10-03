a1e864d24cdc7e4c244160bbabf0e361d8c90371
KarlJay <karljay@email.com>
2021-10-02 23:45:53 -0700

    Initial Commit

---
+++ SwiftUIAnalyticsPage01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAnalyticsPage01
+//
+//  Created by Karl Jay on 10/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAnalyticsPage01/SwiftUIAnalyticsPage01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnalyticsPage01App.swift
+//  SwiftUIAnalyticsPage01
+//
+//  Created by Karl Jay on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnalyticsPage01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
